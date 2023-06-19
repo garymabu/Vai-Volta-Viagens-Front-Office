953df07b9e04104cd0aac63780d22ff9432c7161
João Vitor Coimbra <joao.coimbra@cerc.inf.br>
2023-06-19 04:55:14 -0300

    🐛 fixing home integration

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,4 +1,4 @@
-const homeDomain = 'http://localhost:8080';
+const homeDomain = "http://localhost:8080";
 
 const travels = [
   {
@@ -13,95 +13,148 @@ const travels = [
     origem: "São Paulo",
     destino: "Rio de Janeiro",
     horaPartida: 17,
-    horaChegada: 18
+    horaChegada: 18,
   },
   {
     id: 3,
     origem: "Bahia",
     destino: "Porto Alegre",
     horaPartida: 10,
-    horaChegada: 16
+    horaChegada: 16,
   },
-]
+];
 
+async function FetchLocalizationsAndLoadInPage() {
+  const result = await fetch(`${homeDomain}/v1/localization`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
+  const { content } = await result.json();
+  const localizations = document.querySelector("#localizations");
 
-function FetchFormData(){
-  const origem = document.querySelector('#Origem').value;
-  const destino = document.querySelector('#Destino').value;
-  console.log(origem)
-  console.log(destino)
-  return {
-    origem:origem,
-    destino: destino
-  }
+  content.forEach((localization) => {
+    const option = document.createElement("option");
+    option.dataset.id = localization.id;
+    option.value = localization.cityId;
+    localizations.appendChild(option);
+  });
 }
 
+async function GetPossibleRoutes(startingLocationId, endingLocationId) {
+  const result = await fetch(
+    `${homeDomain}/v1/trip/match-with-layover?departureLocalizationId=${startingLocationId}&destinationLocalizationId=${endingLocationId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    }
+  );
 
+  const routes = await result.json();
 
-async function FetchDataAndRenderCards(){
-  const cardBox = document.getElementById('cardBox')
-  const {
+  return routes;
+}
+
+function FetchFormData() {
+  const origem = document.querySelector(
+    `option[value='${document.querySelector("#Origem").value}']`
+  ).dataset.id;
+
+  const destino = document.querySelector(
+    `option[value='${document.querySelector("#Destino").value}']`
+  ).dataset.id;
+  return {
     origem,
-    destino
-  } = FetchFormData();
+    destino,
+  };
+}
 
-  if(!origem || !destino){
-    return alert("Local de origem ou destino não informados.")
+async function FetchDataAndRenderCards() {
+  const cardBox = document.getElementById("cardBox");
+  const { origem, destino } = FetchFormData();
+  console.log("origem e destino", origem, destino);
+
+  if (!origem || !destino) {
+    return alert("Local de origem ou destino não informados.");
   }
 
-  // const result = await fetch(`${homeDomain}/v1/travel`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Accept': 'application/json',
-  //   },
-  //   body: JSON.stringify(data),
-  // })
+  const routes = await GetPossibleRoutes(origem, destino);
 
-  const searchedTravels = travels.filter((travel) => travel.origem === origem && travel.destino === destino)
+  const main = document.querySelector("main");
+  if (main.querySelector("h2") === null) {
+    const title = document.createElement("h2");
+    title.innerText = "Viagens disponíveis:";
+    main.prepend(title);
+  }
 
-  console.log(searchedTravels)
-  
-  let travelsHTML = searchedTravels.map((travel) => {
-      return `
-        <div id="travelCard" class="travel-card">
-          <div class="origin-destiny-content">
-            <h3 id="Origem">${travel.origem}</h3>
-            <p> → </p>
-            <h3 id="Destino">${travel.destino}</h3>
-          </div>
-          <div>
-            <span id="horaChegada">Horário de partida: ${travel.horaPartida}:00</span>
-            <span id="horaPartida">Horário de chegada: ${travel.horaChegada}:00</span>
-          </div>
-          <div>
-            <button onclick="RedirectToBookingPage()" class="btn-reserve">Clique para saber mais</button>
-          </div>
-        </div>
-      `
-  })
+  while (cardBox.firstChild) {
+    cardBox.removeChild(cardBox.firstChild);
+  }
 
-  if(travelsHTML.length < 1) {
-    travelsHTML[0] =`
-      <div>
-        <h2>Nenhuma viagem encontrada!</h2>
+  routes.forEach((route) => {
+    const card = document.createElement("div");
+    card.classList.add("travel-card");
+    card.id = "travelCard";
+    card.innerHTML = `
+      <div class="origin-destiny-content">
+        <h3 id="Origem">${route.firstTrip.departureLocalization.cityName}</h3>
+        <p> → </p>
+        <h3 id="Destino">${route.lastTrip.arrivalLocalization.cityName}</h3>
       </div>
-    `
-  }
+      <div>
+        <span id="horaChegada">Horário de partida: ${route.firstTrip.departureDatetime}:00</span>
+        <span id="horaPartida">Horário de chegada: ${route.lastTrip.arrivalDatetime}:00</span>
+      </div>
+      <div>
+        <button onclick="RedirectToBookingPage()" class="btn-reserve">Clique para saber mais</button>
+      </div>
+    `;
+    cardBox.appendChild(card);
+  });
+  // const searchedTravels = travels.filter(
+  //   (travel) => travel.origem === origem && travel.destino === destino
+  // );
 
-  let html = travelsHTML.join('')
-  cardBox.innerHTML = html
+  // let travelsHTML = searchedTravels.map((travel) => {
+  //   return `
+  //       <div id="travelCard" class="travel-card">
+  //         <div class="origin-destiny-content">
+  //           <h3 id="Origem">${travel.origem}</h3>
+  //           <p> → </p>
+  //           <h3 id="Destino">${travel.destino}</h3>
+  //         </div>
+  //         <div>
+  //           <span id="horaChegada">Horário de partida: ${travel.horaPartida}:00</span>
+  //           <span id="horaPartida">Horário de chegada: ${travel.horaChegada}:00</span>
+  //         </div>
+  //         <div>
+  //           <button onclick="RedirectToBookingPage()" class="btn-reserve">Clique para saber mais</button>
+  //         </div>
+  //       </div>
+  //     `;
+  // });
+
+  // if (travelsHTML.length < 1) {
+  //   travelsHTML[0] = `
+  //     <div>
+  //       <h2>Nenhuma viagem encontrada!</h2>
+  //     </div>
+  //   `;
+  // }
+
+  // let html = travelsHTML.join("");
+  // cardBox.innerHTML = htmla;
 }
 
 function RedirectToBookingPage() {
   setTimeout(() => {
     window.location.href = `../booking/index.html`;
-  })
+  });
 }
 
-
-console.log(travelsHTML)
-
-FetchDataAndRenderCards()
-
-
+FetchLocalizationsAndLoadInPage();
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,80 +1,73 @@
-const homeDomain = 'http://localhost:8080';
+const homeDomain = "http://localhost:8080";
 
 function FetchFormData() {
-  const nome = document.querySelector('#Nome').value;
-  const idade = Number(document.querySelector('#Idade').value);
-  const username = document.querySelector('#Username').value;
-  const senha = document.querySelector('#Senha').value;
-  const confirmSenha = document.querySelector('#ConfirmSenha').value;
-  const cpf = document.querySelector('#CPF').value;
-  const prof = document.querySelector('#Prof').value;
-  const tel = document.querySelector('#Tel').value;
+  const nome = document.querySelector("#Nome").value;
+  const idade = Number(document.querySelector("#Idade").value);
+  const username = document.querySelector("#Username").value;
+  const senha = document.querySelector("#Senha").value;
+  const confirmSenha = document.querySelector("#ConfirmSenha").value;
+  const cpf = document.querySelector("#CPF").value;
+  const prof = document.querySelector("#Prof").value;
+  const tel = document.querySelector("#Tel").value;
   return {
     name: nome,
     age: idade,
-    login:username,
-    password:senha,
+    login: username,
+    password: senha,
     confirmSenha,
     cpf,
     profession: prof,
-    tell: tel
+    tell: tel,
   };
 }
 
 async function FetchAndPostFormData() {
-  const {
-    password,
-    confirmSenha,
-    ...formData
-  } = FetchFormData();
-  if(password !== confirmSenha)
-    alert('Senhas não coincidem');
+  const { password, confirmSenha, ...formData } = FetchFormData();
+  if (password !== confirmSenha) alert("Senhas não coincidem");
   else {
     const data = {
       ...formData,
-      password
+      password,
     };
 
     const result = await fetch(`${homeDomain}/v1/client`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify(data),
-    })
+    });
 
-    if(result.status === 200)
-    {
-      FetchLoginAndRedirect({login: data.login, password: data.password});
-    }
-    else
-      alert('Erro ao Cadastrar! Por favor verifique seus dados e tente novamente.')
+    if (result.status === 200) {
+      FetchLoginAndRedirect({ login: data.login, password: data.password });
+    } else
+      alert(
+        "Erro ao Cadastrar! Por favor verifique seus dados e tente novamente."
+      );
   }
 }
 
 async function FetchLoginAndRedirect(loginForm) {
-  const result = await fetch(`${homeDomain}/v1/auth`, {
-    method: 'POST',
+  const result = await fetch(`${homeDomain}/v1/auth/client`, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json",
     },
     body: JSON.stringify(loginForm),
-  })
-  if(result.status === 200)
-  {
+  });
+  if (result.status === 200) {
     const token = (await result.json()).token;
-    sessionStorage.setItem('token', token);
-    RedirectToUserHome()
-  }
-  else
-    alert('Erro ao logar! Por favor verifique seus dados e tente novamente.')
+    sessionStorage.setItem("token", token);
+    RedirectToUserHome();
+  } else
+    alert("Erro ao logar! Por favor verifique seus dados e tente novamente.");
 }
 
 async function FetchLoginAndPostLoginData() {
-  const login = document.querySelector('#Login').value;
-  const password = document.querySelector('#SenhaLogin').value;
+  const login = document.querySelector("#Login").value;
+  const password = document.querySelector("#SenhaLogin").value;
 
   const loginForm = {
     login,
@@ -87,5 +80,5 @@ async function FetchLoginAndPostLoginData() {
 function RedirectToUserHome() {
   setTimeout(() => {
     window.location.href = `../user_home/index.html`;
-  })
+  });
 }
